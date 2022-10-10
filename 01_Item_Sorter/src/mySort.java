a664168c54bd797701f600f9b62269ef3ed60e1c
SamuelBoso <samuel.boso@student.htldornbirn.at>
2022-10-10 14:10:02 +0200

    final_sorter

--- 01_Item_Sorter/src/mySort.java
+++ 01_Item_Sorter/src/mySort.java
@@ -5,6 +5,7 @@ public class mySort {
     public static void main(String[] args) {
         int size = 10;
         int[] data = generateDataArray(size);
+
         for (int i = 0; i < size; i++) {
             System.out.println(data[i]);
         }
@@ -15,11 +16,20 @@ public class mySort {
         int[] data = new int[size];
         for (int i = 0; i < data.length; i++) {
             Random random = new Random();
-            data[i] = random.nextInt(100-1)+1;
+            data[i] = random.nextInt(100000-1)+1;
+        }
+        return sortDataArray(data);
+    }
+    public static int[] sortDataArray(int[] data){
+        for (int i = 0; i < data.length; i++) {
+            int sortset = data[i];
+            int j = i - 1;
+            while(j >= 0 && data[j] > sortset){
+                data[j+1] = data[j];
+                j--;
+            }
+            data[j+1] = sortset;
         }
         return data;
-    }
-    public static int[] sortDataArray(int data){
-
     }
 }
